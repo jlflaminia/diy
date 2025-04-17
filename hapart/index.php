ff516913d5136f4d18a88932c9ba626b72d6f065
JL <jlflaminia1@gmail.com>
2025-04-17 16:11:32 +0800

    Add files via upload

---
+++ hapart/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Repair World</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <div class="header">
+        <h1>Master DIY</h1>
+        <div class="navbar">
+            <a href="#">Categories</a>
+            <a href="#">Community</a>
+            <a href="#">News</a>
+            <a href="#">Login</a>
+            <span></span>
+        </div>
+    </div>
+
+    <div class="main">
+        <h2>Explore the World of Repair</h2>
+        <div class="search-box">
+            <input type="text" class="search-input" placeholder='Search' />
+        </div>
+        <button class="button">Find your thing</button>
+    </div>
+
+    <div class="categories">
+    <a href="categories/1.png"><img src="categories/phone.png" alt="Category 1"></a>
+    <a href="categories/2.png"><img src="categories/computer.png" alt="Category 2"></a>
+    <a href="categories/3.png"><img src="categories/household.png" alt="Category 3"></a>
+    <a href="categories/4.png"><img src="categories/apparel.png" alt="Category 4"></a>
+    <a href="categories/5.png"><img src="categories/appliance.png" alt="Category 5"></a>
+</div>
+
+</body>
+</html>
