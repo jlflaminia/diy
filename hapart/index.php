4bf2740e085bafb942649ed8ffcae83ee71e77b8
jlflaminia <jlflaminia1@gmail.com>
2025-04-17 20:57:29 +0800

    up2

--- hapart/index.php
+++ hapart/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Repair World</title>
+    <title>Master DIY</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
@@ -19,20 +19,31 @@
         </div>
     </div>
 
-    <div class="main">
+    <!-- <div class="main">
         <h2>Explore the World of Repair</h2>
         <div class="search-box">
             <input type="text" class="search-input" placeholder='Search' />
         </div>
         <button class="button">Find your thing</button>
+    </div> -->
+
+    <div class="main">
+    <h1>Welcome to Master DIY</h1>
+    
+    <div class="search-box">
+        <h2>Find the best repair guides</h2>
+        <input type="text" class="search-input" placeholder="Search for a device">
+        <button class="find-device">Find Device</button>
     </div>
+    
+    <!-- <button>Learn More</button> -->
 
     <div class="categories">
-    <a href="categories/1.png"><img src="categories/phone.png" alt="Category 1"></a>
-    <a href="categories/2.png"><img src="categories/computer.png" alt="Category 2"></a>
-    <a href="categories/3.png"><img src="categories/household.png" alt="Category 3"></a>
-    <a href="categories/4.png"><img src="categories/apparel.png" alt="Category 4"></a>
-    <a href="categories/5.png"><img src="categories/appliance.png" alt="Category 5"></a>
+    <a href="categories/1.png"><img src="iphone.png" alt="Category 1"></a>
+    <a href="categories/2.png"><img src="monitor.png" alt="Category 2"></a>
+    <a href="categories/3.png"><img src="household-appliance.png" alt="Category 3"></a>
+    <a href="categories/4.png"><img src="home.png" alt="Category 4"></a>
+    <a href="categories/5.png"><img src="hang-clothes.png" alt="Category 5"></a>
 </div>
 
 </body>
